b9036be04b55b05bf213a877b259fd59ef864cc9
ThuKieu26 <kieuthithu04042006@gmail.com>
2025-07-21 00:17:30 +0700

    Cập nhật code mới

--- Lab4/index.php
+++ Lab4/index.php
@@ -1,36 +1,68 @@
-<?php 
-// Require toàn bộ các file khai báo môi trường, thực thi,...(không require view)
+<?php
+require_once './commons/env.php';
+require_once './commons/function.php';
 
-// Require file Common
-require_once './commons/env.php'; // Khai báo biến môi trường
-require_once './commons/function.php'; // Hàm hỗ trợ
-
-// Require toàn bộ file Controllers
-require_once './controllers/ProductController.php';
-require_once './controllers/CategoryController.php';
-// Require toàn bộ file Models
+// Models
 require_once './models/ProductModel.php';
 require_once './models/CategoryModel.php';
+
+// Controllers
+require_once './controllers/ProductController.php';
+require_once './controllers/CategoryController.php';
+
 // Route
 $act = $_GET['act'] ?? '/';
 
+$controllerProduct = new ProductController();
+$controllerCategory = new CategoryController();
 
-// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+switch ($act) {
+    case '/':
+        $controllerProduct->Home();
+        break;
 
-if ($act == '/') {
-    (new ProductController())->Home();
-} else if ($act == 'detail') {
-    (new ProductController())->Detail();
-} else if ($act == 'category') {
-    (new CategoryController())->Show();
-} else if ($act == 'dangky') {
-    require './views/dangky.php';
-} else if ($act == 'dangnhap') {
-    require './views/dangnhap.php';
-} else if ($act == 'gioithieu') {
-    require './views/gioithieu.php';
-} else if ($act == 'lienhe') {
-    require './views/lienhe.php';
-} else {
-    echo "Không tìm thấy trang";
+    case 'product-detail':
+        $controllerProduct->Detail();
+        break;
+
+    case 'category':
+        $controllerCategory->show();
+        break;
+
+    case 'gioithieu':
+    case 'lienhe':
+    case 'dangky':
+    case 'dangnhap':
+        require "./views/{$act}.php";
+        break;
+
+    case 'category-list':
+        $controllerCategory->list();
+        break;
+    case 'category-add':
+        $controllerCategory->add();
+        break;
+    case 'category-edit':
+        $controllerCategory->edit();
+        break;
+    case 'category-delete':
+        $controllerCategory->delete();
+        break;
+
+    case 'product-list':
+        $controllerProduct->list();
+        break;
+    case 'product-add':
+        $controllerProduct->add();
+        break;
+    case 'product-edit':
+        $controllerProduct->edit();
+        break;
+    case 'product-delete':
+        $controllerProduct->delete();
+        break;
+
+    default:
+        echo "Không tìm thấy trang";
+        break;
 }
--- Lab4/views/products/detail.php
+++ Lab4/views/products/detail.php
@@ -1,12 +1,12 @@
 <?php include './views/layouts/header.php'; ?>
 
-<h2 style="margin: 10px 600px;">Chi tiết sản phẩm</h2>
+<h2>Chi tiết sản phẩm</h2>
 
-<p style="margin: 10px 600px;"><strong>Tên:</strong> <?= $product['name'] ?></p>
-<p style="margin: 10px 600px;"><strong>Giá:</strong> <?= number_format($product['price']) ?> VNĐ</p>
-<p style="margin: 10px 600px;"><strong>Mô tả:</strong> <?= $product['description'] ?></p>
-<p style="margin: 10px 600px;"><strong>Danh mục:</strong> <?= $product['category_id'] ?></p>
+<p><strong>Tên:</strong> <?= $product['name'] ?></p>
+<p><strong>Giá:</strong> <?= number_format($product['price']) ?> VNĐ</p>
+<p><strong>Mô tả:</strong> <?= $product['description'] ?></p>
+<p><strong>Danh mục:</strong> <?= $product['category_id'] ?></p>
 
-<a href="index.php?act=product-list" style="margin: 10px 600px;"> Quay lại danh sách</a>
+<a href="index.php?act=product-list">← Quay lại danh sách</a>
 
 <?php include './views/layouts/footer.php'; ?>
